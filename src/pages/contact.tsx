3a17af2ed686c8948dfca1c890c62975a5ea719b
Nagi Williams <nagi132@gmail.com>
2024-06-20 18:23:49 -0400

    chore: Add contact form functionality and styling

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,115 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser';
+
+interface ContactProps {
+    name: string;
+    email: string;
+    message: string;
+}
 
 const Contact: React.FC = () => {
+    const formRef = useRef<HTMLFormElement>(null);
+    const [formState, setFormState] = useState<ContactProps>({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    };
+    const handleFocus = () => { };
+    const handleBlur = () => { };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+        if (formRef.current) {
+            emailjs.sendForm(
+                process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+                process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+                formRef.current,
+                process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+            ).then(() => {
+                setIsLoading(false);
+                // TODO: Show success message
+                // TODO: Hide an alert
+
+                setFormState({ name: '', email: '', message: '' });
+            }).catch((error) => {
+                setIsLoading(false);
+                console.log(error);
+                // TODO: Show error message
+            });
+        }
+    };
+
     return (
-        <div>Contact</div>
+        <section className='relative flex lg:flex-row flex-col max-container'>
+            <div className='flex-1 min-w-[50%] flex flex-col'>
+                <h1 className='head-text'>Get in Touch</h1>
+
+                <form
+                    ref={formRef}
+                    className='w-full flex flex-col gap-7 mt-14'
+                    onSubmit={handleSubmit}
+                >
+                    <label className='text-black-500 font-semibold'>
+                        Name
+                        <input
+                            type='text'
+                            name='name'
+                            className='input'
+                            placeholder='Your Name'
+                            required
+                            value={formState.name}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </label>
+                    <label className='text-black-500 font-semibold'>
+                        Email
+                        <input
+                            type='email'
+                            name='email'
+                            className='input'
+                            placeholder='example@email.com'
+                            required
+                            value={formState.email}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </label>
+                    <label className='text-black-500 font-semibold'>
+                        Message
+                        <textarea
+                            name='message'
+                            rows={4}
+                            className='textarea'
+                            placeholder='Create a message here'
+                            required
+                            value={formState.message}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </label>
+                    <button
+                        type="submit"
+                        className='btn'
+                        disabled={isLoading}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                    >
+                        {isLoading ? ('Sending...') : ('Send Message')}
+                    </button>
+                </form>
+            </div>
+        </section>
     )
 }
 
