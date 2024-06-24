a0bea4455fd51b0a78842c702d222b5625c2dd5c
Nagi Williams <nagi132@gmail.com>
2024-06-24 03:37:42 -0400

    Update projects and experience section

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,42 +1,14 @@
-import { meta, shopify, starbucks, tesla } from "@/assets/images";
-import {
-    car,
-    contact,
-    estate,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    linkedin,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    pricewise,
-    react,
-    redux,
-    sass,
-    snapgram,
-    summiz,
-    tailwindcss,
-    threads,
-    twitter,
-    typescript
-} from "@/assets/icons";
-import css from '@/assets/icons/css.svg';
 interface Skill {
     imageUrl: string;
     name: string;
     type: string;
 }
 
-interface SocialLink {
-    name: string;
-    iconUrl: string;
-    link: string;
-}
+// interface SocialLink {
+//     name: string;
+//     iconUrl: string;
+//     link: string;
+// }
 
 interface Project {
     iconUrl: string;
@@ -97,7 +69,12 @@ export const skills: Skill[] = [
         name: "Tailwind CSS",
         type: "Frontend",
     },
-
+    {
+        imageUrl: '/assets/icons/threejs.svg', 
+        name: "Three.js",
+        type: "Frontend",
+    },
+    
     // Backend Technologies
     {
         imageUrl: '/assets/icons/nodejs.svg',
@@ -170,66 +147,75 @@ export const skills: Skill[] = [
     }
 ];
 
-export const socialLinks: SocialLink[] = [
-    {
-        name: 'twitter',
-        iconUrl: twitter,
-        link: 'https://twitter.com/Ritika_kaurr',
-    },
-    {
-        name: 'GitHub',
-        iconUrl: github,
-        link: 'https://github.com/ritika789',
-    },
-    {
-        name: 'LinkedIn',
-        iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/ritika-kaur-8b60101b5/',
-    }
-];
+// export const socialLinks: SocialLink[] = [
+//     {
+//         name: 'GitHub',
+//         iconUrl: github,
+//         link: 'https://github.com/Nagi132',
+//     },
+//     {
+//         name: 'LinkedIn',
+//         iconUrl: linkedin,
+//         link: 'https://www.linkedin.com/in/nagi1/',
+//     }
+// ];
 
 export const projects: Project[] = [
     {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'CryptoVerse',
-        description: 'Developed a web application for displaying concept & algorithms of crypto currencies, including changes in values & cost, advantages, disadvantages and financial future of Cryptocurrency.',
-        link: 'https://cryptoverse-96eee9.netlify.app/',
+        iconUrl: 'assets/icons/sketch.svg',
+        theme: 'btn-back-orange',
+        name: 'Sketch and Guess',
+        description: 'A real-time drawing and guessing game. Built with React, Node.js, and MongoDB. Real-time communication via Socket.IO.',
+        link: 'https://github.com/cmartinez131/sketch',
     },
     {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Chocolate cafe club',
-        description: 'Savor the sweetness of our Chocolate Cafe Club website, artfully designed with Bootstrap. Explore our menu, events, and more in a visually delightful and responsive online experience. Indulge in the world of chocolate elegance.',
-        link: 'https://chocholatecafeclub.netlify.app/',
-    },
-    {
-        iconUrl: car,
+        iconUrl: 'assets/icons/pingpong.svg',
         theme: 'btn-back-blue',
-        name: 'HooBank',
-        description: 'Designed and built a Modern UI/UX website using React.js & Tailwind CSS inspired by Figma.',
-        link: 'https://modern-ui-ux-web.vercel.app/',
+        name: 'Ping Pong Online',
+        description: 'Multiplayer ping pong game with lobby creation and CPU play. Built with React, Node.js, and Socket.IO, deployed on Heroku.',
+        link: 'https://github.com/Nagi132/ping-pong-online',
     },
     {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'React-codepen-clone',
-        description: 'Built a complete clone of CodePen, Created a sleek text editor in React Explore real-time code rendering, collaborative editing features, and a responsive design that makes coding and sharing projects a breeze.',
-        link: 'https://vxtjt7.csb.app/',
-    },
-    {
-        iconUrl: estate,
+        iconUrl: 'assets/icons/boardgame.svg',
         theme: 'btn-back-black',
-        name: 'Real-Estate Landing page',
-        description: 'Developed a Landing page for Real estate owners now Explore your dream home on our sleek Real Estate Landing Page. Stunning visuals, detailed listings. ',
-        link: 'https://realestate-locofy-livid.vercel.app/',
+        name: 'Gobble Four',
+        description: 'An engaging multiplayer board game inspired by Gobblet Gobblers. Built with React.js and Firebase for real-time gameplay.',
+        link: 'https://gobblet-gobblers-3bb43.web.app/',
     },
     {
-        iconUrl: summiz,
+        iconUrl: 'assets/icons/question.svg',
+        theme: 'btn-back-green',
+        name: 'QuizApp',
+        description: 'Quiz management platform using React, Node.js, and AWS. Features user authentication, Dockerized deployment, and AWS integration.',
+        link: 'https://github.com/Nagi132/quiz-app',
+    },
+    {
+        iconUrl: 'assets/icons/mentor.svg',
+        theme: 'btn-back-pink',
+        name: 'PrePair',
+        description: 'Mentoring platform for STEM students. Developed with HTML, CSS, Bootstrap, Figma, MongoDB, and Node.js/Express. Won 1st place in Funathon 2022.',
+        link: 'https://github.com/gislainm/funathon-2022',
+    },
+    {
+        iconUrl: 'assets/icons/stars.svg',
         theme: 'btn-back-yellow',
-        name: 'Dadjokes - Chrome extension',
-        description: 'This simple Chrome extension delivers random jokes from the internet, ensuring a steady supply of humor right in your browser. Install now for an instant mood lift and a daily dose of dad-approved laughter.',
+        name: 'DirectMedia Layer Particle System',
+        description: 'C++ particle system using SDL, featuring particle movement, collision detection, and generation.',
         link: 'https://github.com/ritika789/dadjokes-extension',
+    },
+    {
+        iconUrl: 'assets/icons/mandelbrot.svg',
+        theme: 'btn-back-red',
+        name: 'Mandelbrot Generato',
+        description: 'Mandelbrot set generator using C++, creating detailed fractal patterns and saving images for visualization.',
+        link: 'https://github.com/Nagi132/mandelbrot_generator',
+    },
+    {
+        iconUrl: 'assets/icons/rgb.svg',
+        theme: 'btn-back-blue',
+        name: 'RGB Color Picker',
+        description: 'C++ RGB color picker tool with GUI, converting selected RGB values to HTML and Hexadecimal formats.',
+        link: 'https://github.com/Nagi132/rgbpicker',
     }
 ];
 export const service: Service[] = [
@@ -261,42 +247,38 @@ export const service: Service[] = [
 
 export const experiences: Experience[] = [
     {
-        title: 'Software Engineer',
-        company_name: 'Tesla',
-        icon: 'assets/images/tesla.png',
-        iconBg: '#ffcccb',
-        date: 'June 2021 - Present',
-        points: [
-            "Developing and maintaing web applications using React.js and other related technologies.",
-            "Collaborating with the design team to create a seamless user experience.",
-            "Implementing security and data protection.",
-            "Participating in the entire application lifecycle, focusing on coding and debugging."
-        ],
-    },
-    {
-        title: 'Software Engineer',
-        company_name: 'Shopify',
-        icon: 'assets/images/shopify.png',
+        title: 'CUNY Tech Prep Fellow',
+        company_name: 'CUNY Tech Prep',
+        icon: 'assets/images/ctp.jpg',
         iconBg: '#accbe1',
-        date: 'June 2020 - June 2021',
+        date: 'June 2023 - June 2024',
         points: [
-            "Developing and maintaing web applications using React.js and other related technologies.",
-            "Collaborating with the design team to create a seamless user experience.",
-            "Implementing security and data protection.",
-            "Participating in the entire application lifecycle, focusing on coding and debugging."
-        ],
+            "Developed and deployed a full-stack application using React, Node.js, and Socket.io.",
+            "Collaborated with peers in an agile environment, practicing Scrum methodology.",
+            "Gained experience in MVC architecture, TDD, and CI/CD pipelines.",
+            "Participated in workshops and coding challenges to enhance technical skills."],
     },
     {
-        title: 'Software Engineer',
-        company_name: 'Starbucks',
-        icon: 'assets/images/starbucks.png',
-        iconBg: '#90ee90',
-        date: 'June 2019 - June 2020',
+        title: 'Bachelor of Arts in Computer Science',
+        company_name: 'Hunter College',
+        icon: 'assets/images/hunter.jpg',
+        iconBg: '#b19cd9',
+        date: 'Graduated May 2024',
         points: [
-            "Developing and maintaing web applications using React.js and other related technologies.",
-            "Collaborating with the design team to create a seamless user experience.",
-            "Implementing security and data protection.",
-            "Participating in the entire application lifecycle, focusing on coding and debugging."
+            "Completed courses in Algorithms, Data Structures, Web Development, and Software Engineering.",
+            "Worked on various projects, including an online multiplayer drawing and guessing game using React.js and Socket.io.",
+            "Participated in hackathons and coding competitions, winning 1st place in the YPstem Hackathon for 'PrePair'."],
+    },
+    {
+        title: 'Associate of Science in Computer Science',
+        company_name: 'LaGuardia Community College',
+        icon: 'assets/images/lgcc.png',
+        iconBg: '#ffcccb',
+        date: 'Graduated May 2021',
+        points: [
+            "Gained a foundational understanding of Object-Oriented Programming (OOP) in C++.",
+            "Deepened knowledge in Data Structures and Algorithms.",
+            "Enhanced understanding of software development principles through projects and coursework."
         ],
     }
 ];
