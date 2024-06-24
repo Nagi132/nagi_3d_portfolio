f78bab18324cbf8d9922b51e98709c2dbf26dc6f
Nagi Williams <nagi132@gmail.com>
2024-06-24 03:32:00 -0400

    Update tailwind.config.ts with new styles and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,8 @@ const config: Config = {
         },
         blue: {
           500: "#2b77e7"
-        }
+        },
+        whiteOpacity60: "rgba(173, 216, 230, 0.9)",
       },
       fontFamily: {
         'work-sans': ['Work Sans', 'sans-serif'],
@@ -31,8 +32,29 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       boxShadow: {
-        card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
-      }
+        card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        'inner-xl': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
+          '50%': { transform: 'none', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        bounce: 'bounce 1s infinite',
+        'fade-in': 'fadeIn 1.5s ease-in-out',
+      },
     },
   },
   plugins: [],
