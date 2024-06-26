389a2f69dce6a397fe8ee0b733d2464eddc8d7aa
Nagi Williams <nagi132@gmail.com>
2024-06-26 09:14:54 -0400

    update CTA component

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -13,10 +13,8 @@ const CTA: React.FC = () => {
             hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 
             font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 
             transition-transform transform hover:scale-105'>
-    Contact Me
-</Link>
-
-
+                Contact Me
+            </Link>
         </section>
     );
 }
