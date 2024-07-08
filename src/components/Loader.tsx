056e19dd868cbd5aa32c549124500a8ea03aacbd
Nagi Williams <nagi132@gmail.com>
2024-07-08 03:43:14 -0400

    Update loader animation styles

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,12 +4,34 @@ import { Html } from '@react-three/drei'
 const Loader: React.FC = () => {
     return (
         <Html>
-            <div className='flex justify-center items-center'>
-                <div className='w-20 h-20 border-2 border-opacity-20 
-                border-blue-500 border-t-blue500 rounded-full animate-spin' />
+            <div className='boxes'>
+                <div className='box'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className='box'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className='box'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className='box'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </Html>
     )
 }
 
-export default Loader 
+export default Loader
